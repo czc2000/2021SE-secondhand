595afa56307ffed62ac0591390ad50c18448890f
czc <574325403@qq.com>
2021-05-06 10:35:18 +0800

    加入了评论删除功能，并给整个网页加入了一个重新刷新的方法

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,11 @@ export default {
   components:{
     appheader
   },
+  provide () {    //父组件中通过provide来提供变量，在子组件中通过inject来注入变量。
+    return {
+      reload: this.reload
+    }
+  },
   data:function (){
     return{
       isRouterAlive:true
